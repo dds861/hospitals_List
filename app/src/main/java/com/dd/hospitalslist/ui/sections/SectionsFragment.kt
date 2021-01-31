0b02c9a9b284abeb01386f93773e8388f019bca1
Daniyar <dds861@gmail.com>
2021-01-31 22:32:28 +0100

    wip

--- app/src/main/java/com/dd/hospitalslist/ui/sections/SectionsFragment.kt
+++ app/src/main/java/com/dd/hospitalslist/ui/sections/SectionsFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.dd.hospitalslist.HospitalApplication
+import com.dd.hospitalslist.R
 import com.dd.hospitalslist.databinding.SectionsFragmentBinding
 
 class SectionsFragment : Fragment() {
@@ -23,12 +24,34 @@ class SectionsFragment : Fragment() {
     private lateinit var binding: SectionsFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding = SectionsFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.inflateMenu(R.menu.menu)
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_back -> {
+                    findNavController().navigateUp()
+                    true
+                }
+                R.id.action_done -> {
+                    // Save profile changes
+                    true
+                }
+                else -> false
+            }
+        }
+
+        val saveItem = binding.toolbar.menu.findItem(R.id.action_back)
+        saveItem.isVisible = false
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -36,8 +59,5 @@ class SectionsFragment : Fragment() {
             val action = SectionsFragmentDirections.actionSectionsFragmentToHospitalFragment()
             findNavController().navigate(action)
         }
-
     }
-
-
 }
