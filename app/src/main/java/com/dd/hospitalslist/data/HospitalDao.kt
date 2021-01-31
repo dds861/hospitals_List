51187df2775233661954ed990ca4f1c840ce013c
Daniyar <dds861@gmail.com>
2021-01-31 23:20:18 +0100

    regions

--- app/src/main/java/com/dd/hospitalslist/data/HospitalDao.kt
+++ app/src/main/java/com/dd/hospitalslist/data/HospitalDao.kt
@@ -1,14 +1,19 @@
 package com.dd.hospitalslist.data
 
+import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Query
 import com.dd.hospitalslist.data.entities.Hospital
+import com.dd.hospitalslist.data.entities.Region
 
 @Dao
 interface HospitalDao {
 
     @Query("SELECT * FROM hospitals")
     fun getHospitals(): DataSource.Factory<Int, Hospital>
+
+    @Query("SELECT * FROM regions")
+    fun getRegions(): LiveData<List<Region>>
 
 }
--- app/src/main/java/com/dd/hospitalslist/data/HospitalRepository.kt
+++ app/src/main/java/com/dd/hospitalslist/data/HospitalRepository.kt
@@ -4,9 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.paging.PagedList
 import androidx.paging.toLiveData
 import com.dd.hospitalslist.data.entities.Hospital
+import com.dd.hospitalslist.data.entities.Region
 
 class HospitalRepository(private val hospitalDao: HospitalDao) {
     fun getHospitals(): LiveData<PagedList<Hospital>> {
         return hospitalDao.getHospitals().toLiveData(pageSize = 1)
+    }
+
+    fun getRegions(): LiveData<List<Region>> {
+        return hospitalDao.getRegions()
     }
 }
