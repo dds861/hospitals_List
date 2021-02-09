bffa981466a86c5a540587b3b9b93fa0156a56b2
Daniyar <dds861@gmail.com>
2021-02-09 17:25:23 +0100

    ui

--- app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalFragment.kt
+++ app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalFragment.kt
@@ -1,19 +1,19 @@
 package com.dd.hospitalslist.ui.hospitals
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.dd.hospitalslist.HospitalApplication
-import com.dd.hospitalslist.R
+import com.dd.hospitalslist.data.CategoryState
+import com.dd.hospitalslist.data.HospitalModel
+import com.dd.hospitalslist.data.RegionState
 import com.dd.hospitalslist.databinding.HospitalsFragmentBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collectLatest
@@ -24,6 +24,7 @@ class HospitalFragment : Fragment() {
     private val TAG = "HospitalFragment"
 
     private lateinit var binding: HospitalsFragmentBinding
+    private lateinit var adapter: HospitalAdapter
 
     private val hospitalViewModel: HospitalViewModel by activityViewModels {
         HospitalViewModelFactory(((activity?.application) as HospitalApplication).repository)
@@ -31,9 +32,9 @@ class HospitalFragment : Fragment() {
 
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = HospitalsFragmentBinding.inflate(inflater, container, false)
         return binding.root
@@ -41,76 +42,138 @@ class HospitalFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        adapter = HospitalAdapter()
 
-        val adapter = HospitalAdapter()
         binding.recyclerviewList.adapter = adapter
         binding.recyclerviewList.layoutManager = LinearLayoutManager(requireContext())
 
+        hospitalViewModel.regionNames.observe(viewLifecycleOwner, { list ->
+            context?.let { context ->
+                ArrayAdapter(
+                    context,
+                    android.R.layout.simple_spinner_item,
+                    list
+                ).also { adapter ->
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    binding.spinnerRegion.adapter = adapter
+                }
+            }
+            binding.spinnerRegion.setSelection(ALL_REGIONS)
+        })
+
+        hospitalViewModel.categoriesName.observe(viewLifecycleOwner, { list ->
+            context?.let { context ->
+                ArrayAdapter(
+                    context,
+                    android.R.layout.simple_spinner_item,
+                    list
+                ).also { adapter ->
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    binding.spinnerCategory.adapter = adapter
+                }
+            }
+            binding.spinnerCategory.setSelection(ALL_CATEGORIES)
+        })
+
+
+
+        binding.spinnerRegion.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+
+                    val selectedItem = binding.spinnerRegion.selectedItem.toString()
+
+                    if (position == ALL_REGIONS) {
+                        hospitalViewModel.hospitalModel =
+                            hospitalViewModel.hospitalModel.copy(
+                                regionName = selectedItem,
+                                regionState = RegionState.ALL
+                            )
+                    } else {
+                        hospitalViewModel.hospitalModel =
+                            hospitalViewModel.hospitalModel.copy(
+                                regionName = selectedItem,
+                                regionState = RegionState.SPECIFIC_ITEM
+                            )
+                    }
+
+
+
+                    setList(
+                        hospitalViewModel.hospitalModel
+
+                    )
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+            }
+
+        binding.spinnerCategory.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+
+                    val selectedItem = binding.spinnerCategory.selectedItem.toString()
+
+                    if (position == ALL_CATEGORIES) {
+                        hospitalViewModel.hospitalModel =
+                            hospitalViewModel.hospitalModel.copy(
+                                categoryName = selectedItem,
+                                categoryState = CategoryState.ALL
+                            )
+                    } else {
+                        hospitalViewModel.hospitalModel =
+                            hospitalViewModel.hospitalModel.copy(
+                                categoryName = selectedItem,
+                                categoryState = CategoryState.SPECIFIC_ITEM
+                            )
+                    }
+
+                    hospitalViewModel.hospitalModel =
+                        hospitalViewModel.hospitalModel.copy(categoryName = selectedItem)
+
+                    setList(
+                        hospitalViewModel.hospitalModel
+
+                    )
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+            }
+
+    }
+
+    companion object {
+        const val ALL_CATEGORIES = 0
+        const val ALL_REGIONS = 5
+    }
+
+
+    private fun setList(hospitalModel: HospitalModel) {
 
 
         lifecycleScope.launch {
             @OptIn(ExperimentalCoroutinesApi::class)
-            hospitalViewModel.hospitals.collectLatest { pagingData ->
+            hospitalViewModel.getHospitals(hospitalModel).collectLatest { pagingData ->
 
                 adapter.submitData(pagingData)
             }
         }
-
-
-
-
-        binding.recyclerviewList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (dy > 0) {
-                    binding.includeHospitalFilter.root.visibility = View.GONE
-                    Log.i(TAG, "onBindViewHolder: Scrolled Down")
-                } else {
-                    binding.includeHospitalFilter.root.visibility = View.VISIBLE
-                    Log.i(TAG, "onBindViewHolder: Scrolled Up")
-                }
-            }
-
-//            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-//                super.onScrollStateChanged(recyclerView, newState)
-//                when (newState) {
-//                    AbsListView.OnScrollListener.SCROLL_STATE_FLING -> {
-//                        // Do something
-//                    }
-//                    AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL -> {
-//                        // Do something
-//                    }
-//                    else -> {
-//                        // Do something
-//                    }
-//                }
-//            }
-        })
-
-        //        binding.btn.setOnClickListener {
-        //            val action = SectionsFragmentDirections.actionSectionsFragmentToHospitalFragment()
-        //            findNavController().navigate(action)
-        //        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.inflateMenu(R.menu.menu)
-        binding.toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.action_night_mode -> {
-                    findNavController().navigateUp()
-                    true
-                }
-
-                else -> false
-            }
-        }
-
-        //        //navigate Up
-        //        binding.toolbar.setNavigationIcon(R.drawable.abc_vector_test)
-        //        binding.toolbar.setNavigationOnClickListener {
-        //            findNavController().navigateUp()
-        //        }
-    }
 }
+
--- app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalViewModel.kt
+++ app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalViewModel.kt
@@ -1,20 +1,25 @@
 package com.dd.hospitalslist.ui.hospitals
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.dd.hospitalslist.data.HospitalModel
 import com.dd.hospitalslist.data.HospitalRepository
 import com.dd.hospitalslist.data.entities.Hospital
 import kotlinx.coroutines.flow.Flow
 
 class HospitalViewModel(private val hospitalRepository: HospitalRepository) : ViewModel() {
     private val TAG = "HospitalViewModel"
-    val hospitals: Flow<PagingData<Hospital>>
-        get() {
-            Log.i(TAG, "hospitalRepository.hospitals: ${hospitalRepository.hospitals}")
-            return hospitalRepository.hospitals.cachedIn(viewModelScope)
-        }
 
+
+    fun getHospitals(hospitalModel: HospitalModel): Flow<PagingData<Hospital>> {
+        return hospitalRepository.getHospitals(hospitalModel).cachedIn(viewModelScope)
+    }
+
+    val regionNames = hospitalRepository.getRegionNames()
+
+    val categoriesName = hospitalRepository.getCategoryNames()
+
+    var hospitalModel: HospitalModel = HospitalModel()
 }
