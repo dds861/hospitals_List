842819b664db3c3239a4a4fb6d0465a779e249e8
Daniyar <dds861@gmail.com>
2021-02-10 10:30:40 +0100

    item button functionality

--- app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalViewHolder.kt
+++ app/src/main/java/com/dd/hospitalslist/ui/hospitals/HospitalViewHolder.kt
@@ -7,6 +7,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -25,8 +26,8 @@ class HospitalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val tvMedicalFacility = itemView.findViewById<TextView>(R.id.tvMedicalFacility)
     private val tvRegion = itemView.findViewById<TextView>(R.id.tvRegion)
     private val tvLocation = itemView.findViewById<TextView>(R.id.tvLocation)
-    private val ivIconPhone = itemView.findViewById<ImageView>(R.id.ivIconPhone)
     private val tvPhone = itemView.findViewById<TextView>(R.id.tvPhone)
+    private val ivIconPhone = itemView.findViewById<ImageView>(R.id.ivIconPhone)
     private val ivLocation = itemView.findViewById<ImageView>(R.id.ivIconLocation)
     private val ivCopy = itemView.findViewById<ImageView>(R.id.ivCopy)
     private val ivShare = itemView.findViewById<ImageView>(R.id.ivShare)
@@ -47,39 +48,46 @@ class HospitalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     init {
 
-        val textToShare = "Название:$tvMedicalFacility, " +
-                "Регион: $tvRegion, " +
-                "Адрес: ${tvLocation.text}, " +
-                "Контакты: ${tvPhone.text}"
 
         ivCopy.setOnClickListener { view ->
             scalingAnimationOf(view)
-            copyToClipboard(view.context, textToShare)
+            copyToClipboard(view.context, getTextToShare())
 
         }
 
         ivShare.setOnClickListener { view ->
             scalingAnimationOf(view)
-            shareText(view.context, textToShare)
+            shareText(view.context, getTextToShare())
         }
 
         ivTelegram.setOnClickListener { view ->
             scalingAnimationOf(view)
+            onClickTelegram(view.context, getTextToShare())
         }
-
 
         ivWhatsApp.setOnClickListener { view ->
             scalingAnimationOf(view)
-            onClickWhatsApp(view.context, textToShare)
+            onClickWhatsApp(view.context, getTextToShare())
         }
 
         ivIconPhone.setOnClickListener { view ->
             scalingAnimationOf(view)
+            dialPhone(view.context, getPhoneNumber())
+        }
 
+        tvPhone.setOnClickListener { view ->
+            scalingAnimationOf(view)
+            dialPhone(view.context, getPhoneNumber())
         }
 
         ivLocation.setOnClickListener { view ->
             scalingAnimationOf(view)
+            shareLocation(view.context, getLocationAddress())
+        }
+
+        tvLocation.setOnClickListener { view ->
+            scalingAnimationOf(view)
+            shareLocation(view.context, getLocationAddress())
         }
 
     }
@@ -89,6 +97,21 @@ class HospitalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         tvRegion.text = hospital.region
         tvLocation.text = hospital.address
         tvPhone.text = hospital.phone?.replace("\\n", "\n")
+    }
+
+    private fun getTextToShare(): String {
+        return "Регион: ${tvRegion.text}, " +
+                "Название: ${tvMedicalFacility.text}, " +
+                "Адрес: ${tvLocation.text}, " +
+                "Контакты: ${tvPhone.text}"
+    }
+
+    private fun getPhoneNumber(): String {
+        return tvPhone.text.toString()
+    }
+
+    private fun getLocationAddress(): String {
+        return tvLocation.text.toString()
     }
 
 
@@ -129,14 +152,68 @@ class HospitalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         try {
             val waIntent = Intent(Intent.ACTION_SEND)
             waIntent.type = "text/plain"
-            val info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA)
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA)
             //Check if package exists or not. If not then code
             //in catch block will be called
             waIntent.setPackage("com.whatsapp")
             waIntent.putExtra(Intent.EXTRA_TEXT, text)
-            context.startActivity(Intent.createChooser(waIntent, "Share with"))
+            context.startActivity(
+                Intent.createChooser(
+                    waIntent,
+                    context.getString(R.string.share_using)
+                )
+            )
         } catch (e: PackageManager.NameNotFoundException) {
-            Toast.makeText(context, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+            Toast.makeText(
+                context,
+                context.getString(R.string.whatsapp_not_installed),
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+    private fun onClickTelegram(context: Context, text: String) {
+        val pm: PackageManager = context.packageManager
+        try {
+            val waIntent = Intent(Intent.ACTION_SEND)
+            waIntent.type = "text/plain"
+            pm.getPackageInfo("org.telegram.messenger", PackageManager.GET_META_DATA)
+            //Check if package exists or not. If not then code
+            //in catch block will be called
+            waIntent.setPackage("org.telegram.messenger")
+            waIntent.putExtra(Intent.EXTRA_TEXT, text)
+            context.startActivity(
+                Intent.createChooser(
+                    waIntent,
+                    context.getString(R.string.share_using)
+                )
+            )
+        } catch (e: PackageManager.NameNotFoundException) {
+            Toast.makeText(
+                context,
+                context.getString(R.string.telegram_not_installed),
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+    private fun dialPhone(context: Context, text: String) {
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$text")
+        context.startActivity(intent)
+    }
+
+    private fun shareLocation(context: Context, text: String) {
+        try {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/maps/search/?api=1&query=$text")
+            )
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(context, context.getString(R.string.error), Toast.LENGTH_SHORT)
                 .show()
         }
     }
